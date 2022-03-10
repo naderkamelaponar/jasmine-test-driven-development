50a9dedb194f3bb32652da5402e2521cbf45aa39
Nader <nader.kamel.the.programmer@gmail.com>
2022-03-10 23:17:53 +0200

    working files

--- src/index.ts
+++ src/index.ts
@@ -10,12 +10,19 @@ import axios from "axios";
 
 /** Create getCountry Function here */
 async function getCountry(countryName: string) {
-  const getApi = await axios("ss ${countryName}");
+  const getApi = await axios(
+    `https://restcountries.com/v2/name/${countryName}`
+  );
+  const data = getApi.data;
+  const capital = data[0]["capital"];
+  const region = data[0]["region"];
+  const numericCode = data[0]["numericCode"];
+  return { capital, region, numericCode };
 }
 /** Create a test for this getRegion function */
 async function getRegionCountries(regionalbloc: string) {
   const getApi = await axios(
-    `https://restcountries.eu/rest/v2/regionalbloc/${regionalbloc}`
+    `https://restcountries.com/v2/regionalbloc/${regionalbloc}`
   );
   const data = getApi.data;
   const countries = [];
@@ -26,7 +33,15 @@ async function getRegionCountries(regionalbloc: string) {
 }
 
 /** Create getRegionCapitals function here */
-async function getRegionCapitals() {}
+async function getRegionCapitals(region: string) {
+  const getApi = await axios(`https://restcountries.com/v2/regionalbloc/nafta`);
+  const data = getApi.data;
+  const capitals = [];
+  for (let i = 0; i < data.length; i++) {
+    capitals.push(data[i].capital);
+  }
+  return capitals;
+}
 export default {
   getCountry,
   getRegionCountries,
--- src/tests/indexSpec.ts
+++ src/tests/indexSpec.ts
@@ -1,3 +1,4 @@
+// بسم الله الرحمن الرحيم
 import countries from "../index";
 
 it("should get basic data on the country canada", async () => {
@@ -10,7 +11,10 @@ it("should get basic data on the country canada", async () => {
 });
 
 /** Add test for getRegionCountries function here */
-
+it("should get the Region Countries", async () => {
+  const data = await countries.getRegionCountries("NAFTA");
+  expect(data).toEqual(["Canada", "Mexico", "United States of America"]);
+});
 it("should get capitals of NAFTA countries", async () => {
   const data = await countries.getRegionCapitals("nafta");
   expect(data).toEqual(["Ottawa", "Mexico City", "Washington, D.C."]);
